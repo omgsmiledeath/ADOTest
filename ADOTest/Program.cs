bea47fe6732123f8d58f6862e429637d69d60f03
omgsmiledeath <araragikyn@gmail.com>
2023-02-16 22:49:13 +0800

    some new expiriense

--- ADOTest/Program.cs
+++ ADOTest/Program.cs
@@ -5,24 +5,32 @@ class Program
 {
     static void Main(string[] args)
     {
+        //string conStr = @"Data Source = /Users/kseniabelaevskaa/Projects/Bases/AdoTest.db"; //mac
+        string conStr = @"Data Source =/tmp/TestDB.sqlite3"; //linux
+        
+        
+        
         try
         {
-            SQLiteConnection con = new SQLiteConnection(@"Data Source = /Users/kseniabelaevskaa/Projects/Bases/AdoTest.db");
+            SQLiteConnection con = new SQLiteConnection(conStr);
             con.Open();
+            
 
            using (SQLiteCommand sqlcom = new SQLiteCommand(con))
            {
                sqlcom.CommandText = @"INSERT INTO Workers (firstName,lastName,phoneNumber) VALUES ('раз','dva','8800553535')";
-               sqlcom.ExecuteNonQuery();
+               //sqlcom.ExecuteNonQuery();
            }
             var da = new SQLiteDataAdapter(@"SELECT * FROM Workers", con);
             var dt = new DataTable();
             da.Fill(dt);
-            
-            SQLiteCommand delc = new SQLiteCommand(@"DELETE FROM Workers where oid ='3'",con);
-
-            delc.ExecuteNonQuery();
-            da.Update(dt);
+            PrintTable(dt);
+            System.Console.WriteLine("/n");
+            Prepare(da,dt,con);
+            //Пробуем модифицировать Row
+            //InsertInTable(dt,"o1","p","g");
+            //DeleteInTable(dt,1);
+            UpdateRefill(dt,da);
             PrintTable(dt);
         }
         catch (Exception e)
@@ -48,6 +56,59 @@ class Program
                 Console.Write($"{cell}\t\t");
             Console.WriteLine();
         }
+    }
+    public static void Prepare (SQLiteDataAdapter da,DataTable dt,SQLiteConnection con)
+    {
+        //SELECT
+        da.SelectCommand = new SQLiteCommand("SELECT * FROM Workers",con);
+        //INSERT
+        string sql = @"INSERT INTO Workers (firstName,lastName,phoneNumber)
+        VALUES (@firstName,@lastName,@phoneNumber);";
+        da.InsertCommand = new SQLiteCommand(sql,con);
+        da.InsertCommand.Parameters.Add("@oid",DbType.Int32,4,"oid");
+        da.InsertCommand.Parameters.Add("@firstName",DbType.String,10,"firstName");
+        da.InsertCommand.Parameters.Add("@lastName",DbType.String,10,"lastName");
+        da.InsertCommand.Parameters.Add("@phoneNumber",DbType.String,4,"phoneNumber");
+        //UPDATE
+        sql = @"UPDATE Workers SET
+        firstName = @firstName,
+        lastName = @lastName,
+        phoneNumber = @phoneNumber
+        WHERE oid = @oid;";
+        da.UpdateCommand = new SQLiteCommand(sql,con);
+        da.UpdateCommand.Parameters.Add("@oid",DbType.Int32,4,"oid");
+        da.UpdateCommand.Parameters.Add("@firstName",DbType.String,10,"firstName");
+        da.UpdateCommand.Parameters.Add("@lastName",DbType.String,10,"lastName");
+        da.UpdateCommand.Parameters.Add("@phoneNumber",DbType.String,4,"phoneNumber");
+        //DELETE
+        sql = @"DELETE FROM WORKERS WHERE oid = @oid";
+        da.DeleteCommand = new SQLiteCommand(sql,con);
+        da.DeleteCommand.Parameters.Add("@oid",DbType.Int32,4,"oid");
+    }
+    public static void InsertInTable(DataTable dt,string firstName,string lastName,string phoneNumber)
+    {
+        DataRow row = dt.NewRow();
+        row["firstName"] = firstName;
+        row["lastName"] = lastName;
+        row["phoneNumber"]= phoneNumber;
+        dt.Rows.Add(row);
+    }
+    public static void DeleteInTable(DataTable dt,int oid)
+    {
+        foreach(DataRow dr in dt.Rows)
+        {
+            if (Int32.Parse(dr["oid"].ToString())==oid){
+                dr.Delete();
+            }
+        }
+    }
+    public static void UpdateRefill(DataTable dt,SQLiteDataAdapter da)
+    {
+        da.Update(dt);
+        dt.Dispose();
+        dt = new DataTable();
+        da.Fill(dt);
+
     }
 }
 
